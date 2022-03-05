469d11a3b645d354eac26495f2ba446667f19621
Nguyen Ba Trang <trangrangsu@gmail.com>
2022-03-05 13:20:09 +0700

    commit video 9,10

--- OnlineChat/app/src/main/java/com/is1427/onlinechat/activities/ChatActivity.java
+++ OnlineChat/app/src/main/java/com/is1427/onlinechat/activities/ChatActivity.java
@@ -3,17 +3,46 @@ package com.is1427.onlinechat.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.is1427.onlinechat.adapters.ChatAdapter;
+import com.is1427.onlinechat.models.ChatMessage;
 import com.is1427.onlinechat.utilities.Constants;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
 
 import com.is1427.onlinechat.R;
 import com.is1427.onlinechat.databinding.ActivityChatBinding;
 import com.is1427.onlinechat.models.User;
+import com.is1427.onlinechat.utilities.PreferenceManager;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class ChatActivity extends AppCompatActivity {
     public @NonNull
     ActivityChatBinding binding;
     private User receiverUser;
+    private List<ChatMessage> chatMessages;
+    private ChatAdapter chatAdapter;
+    private PreferenceManager preferenceManager;
+    private FirebaseFirestore database;
+    private String conversionId = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +52,148 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setListeners();
         loadReceiverDetails();
+        init();
+        listenMessages();
     }
+
+    private void init(){
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        chatMessages = new ArrayList<>();
+        chatAdapter = new ChatAdapter(
+                chatMessages,
+                getBitmapFromEncodedString(receiverUser.image),
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+        database = FirebaseFirestore.getInstance();
+    }
+
+    private void sendMessage(){
+        HashMap<String,Object> message = new HashMap<>();
+        message.put(Constants.KEY_SENDER_ID,preferenceManager.getString(Constants.KEY_USER_ID));
+        message.put(Constants.KEY_RECEIVER_ID,receiverUser.id);
+        message.put(Constants.KEY_MESSAGE,binding.inputMessage.getText().toString());
+        message.put(Constants.KEY_TIMESTAMP,new Date());
+        database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
+        if(conversionId != null){
+            updateConversion(binding.inputMessage.getText().toString());
+        }else{
+            HashMap<String,Object> conversion = new HashMap<>();
+            conversion.put(Constants.KEY_SENDER_ID,preferenceManager.getString(Constants.KEY_USER_ID));
+            conversion.put(Constants.KEY_SENDER_NAME,preferenceManager.getString(Constants.KEY_NAME));
+            conversion.put(Constants.KEY_SENDER_IMAGE,preferenceManager.getString(Constants.KEY_IMAGE));
+            conversion.put(Constants.KEY_RECEIVER_ID,receiverUser.id);
+            conversion.put(Constants.KEY_RECEIVER_NAME,receiverUser.name);
+            conversion.put(Constants.KEY_RECEIVER_IMAGE,receiverUser.image);
+            conversion.put(Constants.KEY_LAST_MESSAGE,binding.inputMessage.getText().toString());
+            conversion.put(Constants.KEY_TIMESTAMP,new Date());
+            addConversion(conversion);
+        }
+        binding.inputMessage.setText(null);
+    }
+
+    private void listenMessages(){
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID,preferenceManager.getString(Constants.KEY_USER_ID))
+                .whereEqualTo(Constants.KEY_RECEIVER_ID,receiverUser.id)
+                .addSnapshotListener(eventListener);
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID,receiverUser.id)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID,preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+
+    }
+
+    private final EventListener<QuerySnapshot> eventListener =  (value,error) -> {
+        if(error != null){
+            return;
+        }
+        if(value != null){
+            int count = chatMessages.size();
+            for(DocumentChange documentChange : value.getDocumentChanges()){
+                if(documentChange.getType() == DocumentChange.Type.ADDED){
+                    ChatMessage chatMessage = new ChatMessage();
+                    chatMessage.senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                    chatMessage.receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                    chatMessage.message = documentChange.getDocument().getString(Constants.KEY_MESSAGE);
+                    chatMessage.dateTime = getReadableDateTime(documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP));
+                    chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                    chatMessages.add(chatMessage);
+                }
+            }
+            Collections.sort(chatMessages,(obj1,obj2) -> obj1.dateObject.compareTo(obj2.dateObject));
+            if(count == 0){
+                chatAdapter.notifyDataSetChanged();
+            }else{
+                chatAdapter.notifyItemRangeInserted(chatMessages.size(),chatMessages.size());
+                binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size() - 1);
+            }
+            binding.chatRecyclerView.setVisibility(View.VISIBLE);
+        }
+        binding.progressBar.setVisibility(View.GONE);
+        if(conversionId == null){
+            checkForConversion();
+        }
+    };
+
+    private Bitmap getBitmapFromEncodedString(String encodedImage){
+        byte[] bytes = Base64.decode(encodedImage,Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+    }
+
     private void loadReceiverDetails(){
         receiverUser=(User) getIntent().getSerializableExtra(com.is1427.onlinechat.utilities.Constants.KEY_USER);
         binding.textName.setText(receiverUser.name);
     }
     private void setListeners(){
         binding.imageBack.setOnClickListener(view -> onBackPressed());
+        binding.layoutSend.setOnClickListener(view -> sendMessage());
     }
+
+    private String getReadableDateTime(Date date){
+        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
+    }
+
+    private void addConversion(HashMap<String,Object> conversion){
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .add(conversion)
+                .addOnSuccessListener(documentReference -> conversionId = documentReference.getId());
+    }
+
+    private void updateConversion(String message){
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_CONVERSATIONS).document(conversionId);
+        documentReference.update(
+                Constants.KEY_LAST_MESSAGE,message,
+                Constants.KEY_TIMESTAMP,new Date()
+        );
+    }
+
+    private void checkForConversion(){
+        if(chatMessages.size() != 0){
+            checkForConversionRemotely(
+                    preferenceManager.getString(Constants.KEY_USER_ID),
+                    receiverUser.id
+            );
+            checkForConversionRemotely(
+                    receiverUser.id,
+                    preferenceManager.getString(Constants.KEY_USER_ID)
+            );
+        }
+    }
+
+    private void checkForConversionRemotely(String senderId, String receiverId){
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .whereEqualTo(Constants.KEY_SENDER_ID,senderId)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID,receiverId)
+                .get()
+                .addOnCompleteListener(conversionCompleteListener);
+    }
+
+    private final OnCompleteListener<QuerySnapshot> conversionCompleteListener = task -> {
+      if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+          DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+          conversionId = documentSnapshot.getId();
+      }
+    };
 }
---
+++ OnlineChat/app/src/main/java/com/is1427/onlinechat/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.is1427.onlinechat.listeners;
+
+import com.is1427.onlinechat.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
