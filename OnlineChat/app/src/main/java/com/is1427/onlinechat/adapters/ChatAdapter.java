474a97f979bfc2bb74a7d33c5b5975d0c5f9398b
nguyenmanhsonbg <57060858+nguyenmanhsonbg@users.noreply.github.com>
2022-03-06 21:48:35 +0700

    Notification (tutoriol 12)

--- OnlineChat/app/src/main/java/com/is1427/onlinechat/adapters/ChatAdapter.java
+++ OnlineChat/app/src/main/java/com/is1427/onlinechat/adapters/ChatAdapter.java
@@ -15,12 +15,15 @@ import java.util.List;
 
 public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final List<ChatMessage> chatMessage;
-    private final Bitmap receiverProfileImage;
+    private Bitmap receiverProfileImage;
 
     private final String senderId;
     public static final int VIEW_TYPE_SENT = 1;
     public static final int VIEW_TYPE_RECEIVED = 2;
 
+    public void setReceiverProfileImage(Bitmap bitmap){
+        receiverProfileImage = bitmap;
+    }
     public ChatAdapter(List<ChatMessage> chatMessage, Bitmap receiverProfileImage, String senderId) {
         this.chatMessage = chatMessage;
         this.receiverProfileImage = receiverProfileImage;
@@ -96,7 +99,10 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         void setData(@NonNull ChatMessage chatMessage, Bitmap receiverProfileImage) {
                 binding.textMessage.setText(chatMessage.message.trim());
                 binding.textDateTime.setText(chatMessage.dateTime);
-                binding.imageProfile.setImageBitmap(receiverProfileImage);
+                if(receiverProfileImage != null){
+                    binding.imageProfile.setImageBitmap(receiverProfileImage);
+                }
+
         }
 
     }
